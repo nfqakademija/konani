6e22aed2287ee84fa277ed030a54e2c246fa00aa
bhd222 <evaldas.kocys@gmail.com>
2014-11-30 23:58:28 +0200

    Added linked public channel protection

--- src/Konani/VideoBundle/Controller/VideoController.php
+++ src/Konani/VideoBundle/Controller/VideoController.php
@@ -3,6 +3,7 @@
 namespace Konani\VideoBundle\Controller;
 
 use Doctrine\ORM\NoResultException;
+use Konani\VideoBundle\Entity\Video;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Google_Service_YouTube;
@@ -13,6 +14,11 @@ use Google_Service_YouTube_VideoSnippet;
 use Google_Service_YouTube_VideoStatus;
 use Google_Service_YouTube_Video;
 use Google_Http_MediaFileUpload;
+
+use Google_Service_YouTube_ChannelSnippet;
+use Google_Service_YouTube_Channel;
+
+
 
 use Konani\VideoBundle\Entity\File;
 
@@ -110,28 +116,87 @@ class VideoController extends Controller
 
             $client->authenticate($code);
             $this->get('session')->set('token', $client->getAccessToken());
-            $this->redirect($my_client->getRedirect());
+            return $this->redirect($my_client->getRedirect());
         }
 
         if ($this->get('session')->get('token')) {
             $client->setAccessToken($this->get('session')->get('token'));
         }
 
+        $return = array();
+
         if ($client->getAccessToken()) {
-            return $this->render('KonaniVideoBundle:Default:authenticateGoogle.html.twig');
+
+            $youtube = new Google_Service_YouTube($client);
+
+            try {
+                $channelsResponse = $youtube->channels->listChannels('status', array(
+                    'mine' => 'true',
+                ));
+
+                $return['channelLinked'] = $channelsResponse['items'][0]['status']->getIsLinked();
+                $return['channelPrivacy'] = $channelsResponse['items'][0]['status']->getPrivacyStatus();
+
+            } catch (Google_Service_Exception $e) {
+                $return['errors']['service'] = htmlspecialchars($e->getMessage());
+            } catch (Google_Exception $e) {
+                $return['errors']['client'] = htmlspecialchars($e->getMessage());
+            }
+
+            $this->get('session')->set('token', $client->getAccessToken());
+
+            return $this->render('KonaniVideoBundle:Default:authenticateGoogle.html.twig', array(
+                'params' => $return,
+            ));
         } else {
             $state = mt_rand();
             $client->setState($state);
             $this->get('session')->set('state', $state);
 
-            $authUrl = $client->createAuthUrl();
+            $return['authUrl'] = $client->createAuthUrl();
 
             return $this->render('KonaniVideoBundle:Default:authenticateGoogle.html.twig', array(
-                    'authUrl' => $authUrl,
+                    'params' => $return,
                 ));
         }
 
 
+    }
+    public function createYoutubeChannelAction()
+    {
+        $my_client = $this->get('google_client');
+        $client = $my_client->getGoogleClient();
+
+        $return = array();
+
+        if ($client->getAccessToken()) {
+
+            $youtube = new Google_Service_YouTube($client);
+
+            try {
+                $channelsResponse = $youtube->channels->listChannels('status', array(
+                        'mine' => 'true',
+                    ));
+
+                // Now update channel privacy to public and link a channel
+
+                $return['channelLinked'] = $channelsResponse['items'][0]['status']->getIsLinked();
+                $return['channelPrivacy'] = $channelsResponse['items'][0]['status']->getPrivacyStatus();
+
+            } catch (Google_Service_Exception $e) {
+                $return['errors']['service'] = htmlspecialchars($e->getMessage());
+            } catch (Google_Exception $e) {
+                $return['errors']['client'] = htmlspecialchars($e->getMessage());
+            }
+
+            $this->get('session')->set('token', $client->getAccessToken());
+
+            return $this->render('KonaniVideoBundle:Default:createYoutubeChannel.html.twig', array(
+                    'params' => $return,
+                ));
+        } else {
+            return $this->redirect($this->generateUrl('video_authenticate_google'));
+        }
     }
     public function uploadToYoutubeAction($id)
     {
@@ -148,68 +213,76 @@ class VideoController extends Controller
 
         if ($client->getAccessToken()) {
             try{
-                $file = $this->getDoctrine()
-                    ->getRepository('KonaniVideoBundle:File')
-                    ->find($id);
 
-                if (!$file) {
-                    throw $this->createNotFoundException(
-                        'No video found for id '.$id
+                $channelsResponse = $youtube->channels->listChannels('status', array(
+                        'mine' => 'true',
+                    ));
+
+                if ($channelsResponse['items'][0]['status']->getIsLinked() && $channelsResponse['items'][0]['status']->getPrivacyStatus() == 'public') {
+
+                    $file = $this->getDoctrine()
+                        ->getRepository('KonaniVideoBundle:File')
+                        ->find($id);
+
+                    if (!$file) {
+                        throw $this->createNotFoundException(
+                            'No video found for id ' . $id
+                        );
+                    }
+
+                    $videoPath = $file->getAbsolutePath();
+
+                    // Create a snipet with title, description, tags and category id
+                    $snippet = new Google_Service_YouTube_VideoSnippet();
+                    $snippet->setTitle($file->GetName());
+                    $snippet->setDescription("Another description");
+                    $snippet->setTags(array("Snowboarder", "Symfony", "Google", "Youtube"));
+
+                    // Numeric video category. See
+                    // https://developers.google.com/youtube/v3/docs/videoCategories/list
+                    $snippet->setCategoryId("22");
+
+                    // Create a video status with privacy status. Options are "public", "private" and "unlisted".
+                    $status = new Google_Service_YouTube_VideoStatus();
+                    $status->privacyStatus = "private";
+                    // Associate the snippet and status objects with a new video resource.
+                    $video = new Google_Service_YouTube_Video();
+                    $video->setSnippet($snippet);
+                    $video->setStatus($status);
+                    // Specify the size of each chunk of data, in bytes. Set a higher value for
+                    // reliable connection as fewer chunks lead to faster uploads. Set a lower
+                    // value for better recovery on less reliable connections.
+                    $chunkSizeBytes = 1 * 1024 * 1024;
+                    // Setting the defer flag to true tells the client to return a request which can be called
+                    // with ->execute(); instead of making the API call immediately.
+                    $client->setDefer(true);
+                    // Create a request for the API's videos.insert method to create and upload the video.
+                    $insertRequest = $youtube->videos->insert("status,snippet", $video);
+                    // Create a MediaFileUpload object for resumable uploads.
+                    $media = new Google_Http_MediaFileUpload(
+                        $client,
+                        $insertRequest,
+                        'video/*',
+                        null,
+                        true,
+                        $chunkSizeBytes
                     );
+                    $media->setFileSize(filesize($videoPath));
+                    // Read the media file and upload it chunk by chunk.
+                    $status = false;
+                    $handle = fopen($videoPath, "rb");
+                    while (!$status && !feof($handle)) {
+                        $chunk = fread($handle, $chunkSizeBytes);
+                        $status = $media->nextChunk($chunk);
+                    }
+                    fclose($handle);
+                    // If you want to make other calls after the file upload, set setDefer back to false
+                    $client->setDefer(false);
+
+                    $return['video'] = $status;
+                } else {
+                    return $this->redirect($this->generateUrl('video_authenticate_google'));
                 }
-
-                $videoPath = $file->getAbsolutePath();
-
-                // Create a snipet with title, description, tags and category id
-                $snippet = new Google_Service_YouTube_VideoSnippet();
-                $snippet->setTitle($file->GetName());
-                $snippet->setDescription("Another description");
-                $snippet->setTags(array("Snowboarder", "Symfony", "Google", "Youtube"));
-
-                // Numeric video category. See
-                // https://developers.google.com/youtube/v3/docs/videoCategories/list
-                $snippet->setCategoryId("22");
-
-                // Create a video status with privacy status. Options are "public", "private" and "unlisted".
-                $status = new Google_Service_YouTube_VideoStatus();
-                $status->privacyStatus = "private";
-                // Associate the snippet and status objects with a new video resource.
-                $video = new Google_Service_YouTube_Video();
-                $video->setSnippet($snippet);
-                $video->setStatus($status);
-                // Specify the size of each chunk of data, in bytes. Set a higher value for
-                // reliable connection as fewer chunks lead to faster uploads. Set a lower
-                // value for better recovery on less reliable connections.
-                $chunkSizeBytes = 1 * 1024 * 1024;
-                // Setting the defer flag to true tells the client to return a request which can be called
-                // with ->execute(); instead of making the API call immediately.
-                $client->setDefer(true);
-                // Create a request for the API's videos.insert method to create and upload the video.
-                $insertRequest = $youtube->videos->insert("status,snippet", $video);
-                // Create a MediaFileUpload object for resumable uploads.
-                $media = new Google_Http_MediaFileUpload(
-                    $client,
-                    $insertRequest,
-                    'video/*',
-                    null,
-                    true,
-                    $chunkSizeBytes
-                );
-                $media->setFileSize(filesize($videoPath));
-                // Read the media file and upload it chunk by chunk.
-                $status = false;
-                $handle = fopen($videoPath, "rb");
-                while (!$status && !feof($handle)) {
-                    $chunk = fread($handle, $chunkSizeBytes);
-                    $status = $media->nextChunk($chunk);
-                }
-                fclose($handle);
-                // If you want to make other calls after the file upload, set setDefer back to false
-                $client->setDefer(false);
-
-                $return['video'] = $status;
-
-            // Reikia pirma useriui funkcijos - susikurti youtube cahnneli, paskuj uploadinti
 
             } catch (Google_Service_Exception $e) {
                 $return['errors']['service'] = htmlspecialchars($e->getMessage());
@@ -224,5 +297,19 @@ class VideoController extends Controller
         }
 
         return $this->render('KonaniVideoBundle:Default:uploadToYoutube.html.twig', array( 'params' => $return));
+    }
+    public function newTagAction()
+    {
+        $video = new Video();
+        $video->setUser($this->getUser());
+
+        $form = $this->createFormBuilder($video)
+            ->add('latitude')
+            ->add('longitude')
+            ->add('name')
+            ->add('description')
+
+            ->add('save','submit')
+            ->getForm();
     }
 }
