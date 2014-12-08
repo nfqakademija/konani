a274a2627300eee9f44448ae29189e654c1ad350
bhd222 <evaldas.kocys@gmail.com>
2014-12-08 21:50:09 +0200

    Restored deleted code.

--- app/Resources/scripts/map.js
+++ app/Resources/scripts/map.js
@@ -1,3 +1,29 @@
-/**
- * Created by evaldas on 14.12.8.
- */
+'use strict';
+function bothFilled()
+{
+    var lat = $('#form_latitude').val();
+    var lng = $('#form_longitude').val();
+
+    if ((lat.length === 0)  || (lng.length === 0) ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+$( '#newTag' ).submit(function( event ) {
+    if (!bothFilled()) {
+        $('.alert').addClass('alert-danger').removeClass('hidden');
+        event.preventDefault();
+    } else {
+        $('.alert').addClass('hidden');
+    }
+});
+
+$( document ).ajaxStart(function() {
+    $( '#ajax_loading' ).show();
+});
+
+$( document ).ajaxStop(function() {
+    $( '#ajax_loading' ).hide();
+});
