719e79606540757f8de36226256268e4b87b3d83
bhd222 <evaldas.kocys@gmail.com>
2014-12-09 23:13:28 +0200

    Added map geolocation search for video tagging.

--- app/Resources/scripts/map.js
+++ app/Resources/scripts/map.js
@@ -1,4 +1,9 @@
 'use strict';
+
+/* global map */
+/* global google */
+var geocoder = new google.maps.Geocoder();
+
 function bothFilled()
 {
     var lat = $('#form_latitude').val();
@@ -11,19 +16,43 @@ function bothFilled()
     }
 }
 
-$( '#newTag' ).submit(function( event ) {
-    if (!bothFilled()) {
-        $('.alert').addClass('alert-danger').removeClass('hidden');
+$(document).ready(function() {
+
+    $('#newTag').submit(function (event) {
+        if (!bothFilled()) {
+            $('.alert').addClass('alert-danger').removeClass('hidden');
+            event.preventDefault();
+        } else {
+            $('.alert').addClass('hidden');
+        }
+    });
+
+    $(document).ajaxStart(function () {
+        $('#ajax_loading').show();
+    });
+
+    $(document).ajaxStop(function () {
+        $('#ajax_loading').hide();
+    });
+
+    $('#search_form').submit(function(event) {
         event.preventDefault();
-    } else {
-        $('.alert').addClass('hidden');
-    }
-});
+        geocoder.geocode({ 'address': $('#search_query').val() }, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
 
-$( document ).ajaxStart(function() {
-    $( '#ajax_loading' ).show();
-});
+                var lat = results[0].geometry.location.lat().toString().substr(0, 12);
+                var lng = results[0].geometry.location.lng().toString().substr(0, 12);
 
-$( document ).ajaxStop(function() {
-    $( '#ajax_loading' ).hide();
+                var myLatLng = new google.maps.LatLng(lat, lng);
+
+                $('#search_query').val(results[0].formatted_address);
+
+                map.setCenter(myLatLng);
+                map.setZoom(14);
+
+            } else {
+                $('#location-search').addClass('red');
+            }
+        });
+    });
 });
